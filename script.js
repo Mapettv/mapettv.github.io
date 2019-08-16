71ec39b4a35e3e3275e463235812256a940ab527
cholodymedia <cholodymedia@gmail.com>
2019-08-16 15:02:39 +0200

    Add game name to Twitch Tab

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let json_data;
 
+// let userID = "25452510";
+let userID = "106512746";
+
 function CheckOnlineStatus() {
   $.ajax({
-    url: "https://api.twitch.tv/kraken/streams/106512746",
+    url: "https://api.twitch.tv/kraken/streams/" + userID,
     dataType: "json",
     headers: {
       Accept: "application/vnd.twitchtv.v5+json",
@@ -33,6 +36,8 @@ const streamBox = {
     root.appendChild(stream_box);
     let title_box = document.createElement("div");
     title_box.classList.add("title-box");
+    let title_box_top = document.createElement("div");
+    title_box_top.classList.add("title-box-top");
     let stream_title = document.createElement("div");
     stream_title.classList.add("stream-title");
     let title_icon = document.createElement("div");
@@ -47,7 +52,7 @@ const streamBox = {
       title_text.innerHTML = "Stream Offline";
     }
     stream_title.appendChild(title_text);
-    title_box.appendChild(stream_title);
+    title_box_top.appendChild(stream_title);
     let stream_views = document.createElement("div");
     stream_views.classList.add("stream-views");
     let views_icon = document.createElement("div");
@@ -62,14 +67,42 @@ const streamBox = {
       views_number.innerHTML = "0";
     }
     stream_views.appendChild(views_number);
-    title_box.appendChild(stream_views);
+    title_box_top.appendChild(stream_views);
+    let title_box_bottom = document.createElement("div");
+    title_box_bottom.classList.add("title-box-bottom");
+    let game_icon = document.createElement("div");
+    game_icon.classList.add("game-icon");
+    game_icon.innerHTML = '<i class="fas fa-gamepad"></i>';
+    title_box_bottom.appendChild(game_icon);
+    let game_name = document.createElement("div");
+    game_name.classList.add("game-name");
+    if (json_data.stream != null) {
+      game_name.innerHTML = json_data.stream.game;
+    } else {
+      game_name.innerHTML = "Offline";
+    }
+    title_box_bottom.appendChild(game_name);
+    title_box.appendChild(title_box_top);
+    title_box.appendChild(title_box_bottom);
+    let space = document.createElement("div");
+    space.classList.add("space");
+    title_box.appendChild(space);
     root.appendChild(title_box);
-    var options = {
-      width: "100%",
-      height: "100%",
-      channel: "mapettv",
-      muted: true
-    };
+    if (json_data.stream != null) {
+      var options = {
+        width: "100%",
+        height: "100%",
+        channel: json_data.stream.channel.name,
+        muted: true
+      };
+    } else {
+      var options = {
+        width: "100%",
+        height: "100%",
+        channel: "mapettv",
+        muted: true
+      };
+    }
     var player = new Twitch.Player("player", options);
   }
 };
